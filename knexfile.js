381bb0ae3ea848ea915a4231bd888a7c10b29bd6
khwanchaiwill <64489050+khwanchaiwill@users.noreply.github.com>
2020-08-23 18:36:52 -0400

    fix api

--- knexfile.js
+++ knexfile.js
@@ -33,7 +33,10 @@ module.exports = {
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
     }
   }
 
